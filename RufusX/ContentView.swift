0a08c33a6f986bef0013a9b6ba5132880d11c848
陳德生 <111033412+KoukeNeko@users.noreply.github.com>
2025-11-20 19:19:02 +0800

    Initial Commit

---
+++ RufusX/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RufusX
+//
+//  Created by 陳德生 on 2025/11/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RufusX/RufusXApp.swift
@@ -0,0 +1,17 @@
+//
+//  RufusXApp.swift
+//  RufusX
+//
+//  Created by 陳德生 on 2025/11/20.
+//
+
+import SwiftUI
+
+@main
+struct RufusXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
